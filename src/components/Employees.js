89187e49cc5df7cc8903d34e6a6fa04c47ee820e
GeethuBosco <geethubosco.com>
2023-11-26 22:05:55 +0530

    React Crud Employees

---
+++ src/components/Employees.js
@@ -0,0 +1,20 @@
+const Employees = [
+    {
+        id:"1",
+        Name:"Geethu",
+        Age:"25"
+    },{
+        id:"2",
+        Name:"Devika",
+        Age:"24"
+    },{
+        id:"3",
+        Name:"Lakshmi",
+        Age:"22"
+    },{
+        id:"4",
+        Name:"Thushara",
+        Age:"25"
+    },
+]
+export default Employees;
